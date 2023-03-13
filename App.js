c499c546c238a638771e9db399f97dc188e4ac2f
caiossilva <caiosouzasilva13650@gmail.com>
2023-03-12 23:00:27 -0300

    top bar add

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 
-import { View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import Keyboard from './Components/Keyboard';
 
 export default function App() {
   return (
     <View className="flex-1 bg-black">
+      <StatusBar barStyle={'default'}/>
       <Keyboard/>
     </View>
   );
--- Components/Display.js
+++ Components/Display.js
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 
 export default function Display({caracters}) {
   return (
-    <View className="bg-zinc-900 w-screen h-40 items-end mt-36 justify-center">
+    <View className="bg-zinc-900 w-screen h-40 items-end mt-[50px] justify-center">
         <Text className="text-white font-bold text-3xl">
             {caracters}
         </Text>
